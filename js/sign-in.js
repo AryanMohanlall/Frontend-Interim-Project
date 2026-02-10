848f3b7813b9406a0ae8f9c6c85ae6d8f19e6588
AryanMohanlall <u23565536@tuks.co.za>
2026-02-10 14:12:29 +0200

    Sign in UI mostly complete

---
+++ js/sign-in.js
@@ -0,0 +1,16 @@
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const signInButton = document.getElementById('sign-in-button');
+const errorLabel = document.querySelector('.errorLabel');
+
+
+const handleSignIn = ()=>{
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    errorLabel.textContent = (usernameValue === '' ? 'Username cannot be empty' : "");
+    errorLabel.textContent += (passwordValue === '' ? ' Password cannot be empty' : "");
+
+    if(usernameValue !== '' && passwordValue !== ''){
+        
+    }
+}
