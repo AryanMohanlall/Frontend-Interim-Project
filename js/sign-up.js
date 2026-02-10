3ae239502191382ce72166235b0aa64be6a56128
AryanMohanlall <u23565536@tuks.co.za>
2026-02-10 21:01:25 +0200

    Sign up functionality

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,13 +3,25 @@ const password = document.getElementById('password');
 const signInButton = document.getElementById('sign-in-button');
 const errorLabel = document.querySelector('.errorLabel');
 
-const handleSignUp = ()=>{
+
+const gotoSignIn = () => {
+    window.location.href = 'sign-in.html';
+}
+
+const handleSignUp = async()=>{
     const usernameValue = username.value.trim();
     const passwordValue = password.value.trim();
     errorLabel.textContent = (usernameValue === '' ? 'Username cannot be empty' : "");
     errorLabel.textContent += (passwordValue === '' ? ' Password cannot be empty' : "");
 
     if(usernameValue !== '' && passwordValue !== ''){
-
+        const user = localStorage.getItem(usernameValue);
+        if(user){
+            errorLabel.textContent = 'Username already exists';
+        } else {
+            localStorage.setItem(usernameValue, passwordValue);
+            document.querySelector('.alert-modal').style.display = 'block';
+            window.location.href = 'sign-in.html';
+        }
     }
 }
