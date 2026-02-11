6798e02db9519575111d638a505810aacef15ed2
AryanMohanlall <u23565536@tuks.co.za>
2026-02-11 12:54:34 +0200

    Send message functionality complete

--- js/home.js
+++ js/home.js
@@ -8,9 +8,6 @@ addEventListener('DOMContentLoaded', ()=>{
 });
 
 
-const contactCard = (data) => {
-    alert(data.id);
-}
 
 const createContactCards = async () => {
     const data = await fetch('../db/users.json');
@@ -23,7 +20,7 @@ const createContactCards = async () => {
         newContact.classList.add('contact');
         newContact.id = users.users[u].username;
         
-        newContact.onclick = function() { contactCard(this) }; 
+        newContact.onclick = function() { populateChatArea(this) }; 
 
         const imgFrame = document.createElement('div');
         imgFrame.classList.add('imgFrame');
@@ -41,3 +38,137 @@ const createContactCards = async () => {
     }
 }
 
+const addSenderMessage = (message)=>{
+    console.log(message);
+    const messageLog = document.querySelector('.message-log');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message', 'sender');
+    
+    const messageLabel = document.createElement('label');
+    messageLabel.textContent = message.label;
+
+    const strongElement = document.createElement('strong');
+    strongElement.appendChild(messageLabel);
+    messageElement.appendChild(strongElement);
+
+    const messageContent = document.createElement('p');
+    messageContent.textContent = message.content;
+    messageElement.appendChild(messageContent);
+
+
+    const messageTimestamp = document.createElement('label');
+    messageTimestamp.id = "timestamp";
+    messageTimestamp.textContent = message.timestamp;
+    messageElement.appendChild(messageTimestamp);
+
+    messageLog.appendChild(messageElement);
+}
+
+const addReceiverMessage = (message)=>{
+    console.log(message);
+    const messageLog = document.querySelector('.message-log');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message', 'receiver');
+    
+    const messageLabel = document.createElement('label');
+    messageLabel.textContent = message.label;
+
+    const strongElement = document.createElement('strong');
+    strongElement.appendChild(messageLabel);
+    messageElement.appendChild(strongElement);
+
+    const messageContent = document.createElement('p');
+    messageContent.textContent = message.content;
+    messageElement.appendChild(messageContent);
+
+
+    const messageTimestamp = document.createElement('label');
+    messageTimestamp.id = "timestamp";
+    messageTimestamp.textContent = message.timestamp;
+    messageElement.appendChild(messageTimestamp);
+
+    messageLog.appendChild(messageElement);
+}
+
+const populateChatArea = (contact) => {
+    document.querySelector('.message-log').replaceChildren();
+
+    localStorage.setItem('currentChat', contact.id ? contact.id : localStorage.getItem('currentChat'));
+
+    const chatTitle = document.querySelector('.chat-header h3');
+    chatTitle.textContent = localStorage.getItem('currentChat');
+
+    const chatID = getChatID(localStorage.getItem('currentUsername') + document.querySelector('.chat-header h3').textContent);
+
+    if(localStorage.getItem(chatID)){
+        const messages = JSON.parse(localStorage.getItem(chatID));
+        console.log(messages);
+
+        for(let m in messages.messages){
+            
+            if(messages.messages[m].content !== ""){
+                messages.messages[m].label !== localStorage.getItem('currentUsername') ? addSenderMessage(messages.messages[m]) : addReceiverMessage(messages.messages[m]);
+            }
+        }
+
+    }else{
+        localStorage.setItem(chatID, JSON.stringify({
+            messages:[
+            message={
+                label:chatTitle.textContent,
+                content:"",
+                timestamp:new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            }
+        ]}));
+    }
+
+}
+
+const getChatID = (str)=>{
+    str = str.split(' ');
+    str.sort();
+    return str.join('');
+}
+
+const createChat = (chatID)=>{
+            localStorage.setItem(chatID, JSON.stringify({
+            messages:[
+            message={
+                label:chatTitle.textContent,
+                content:"",
+                timestamp:new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            }
+        ]}));
+}
+
+const addMessageToChat = (messageObj, chatID)=>{
+    const chat = JSON.parse(localStorage.getItem(chatID));
+    chat.messages.push(messageObj);
+    localStorage.setItem(chatID, JSON.stringify(chat));
+}
+
+const sendMessage = ()=>{
+        const chatID = getChatID(localStorage.getItem('currentUsername') + localStorage.getItem('currentChat'));
+
+        if(!localStorage.getItem(chatID)){
+            createChat(chatID);
+        }
+            
+        const messages = JSON.parse(localStorage.getItem(chatID));
+
+        const messageInput = document.querySelector('.message-box input');
+        const message = messageInput.value.trim();
+        if(message !== ''){
+            console.log(message);
+            console.log(messages);
+            const messageObj = {
+                label:localStorage.getItem('currentUsername'),
+                content:message,
+                timestamp:new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            }
+
+            addMessageToChat(messageObj, chatID);
+            populateChatArea(localStorage.getItem('currentChat'));
+            messageInput.value = '';
+        }
+}
