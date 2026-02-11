cfbb15576e207541c0138a402a9ecdc7fca9631f
AryanMohanlall <u23565536@tuks.co.za>
2026-02-11 11:50:10 +0200

    Populate contact list is now working

--- js/home.js
+++ js/home.js
@@ -1,3 +1,43 @@
 if(!localStorage.getItem('isLoggedIn')){
     window.location.href = 'sign-in.html';
 }
+
+addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('banner-title').textContent = localStorage.getItem('currentUsername');
+    createContactCards();
+});
+
+
+const contactCard = (data) => {
+    alert(data.id);
+}
+
+const createContactCards = async () => {
+    const data = await fetch('../db/users.json');
+    const users = await data.json();
+    
+    for(let u in users.users) {
+        const contactList = document.querySelector('.contact-list');
+        
+        const newContact = document.createElement('div'); 
+        newContact.classList.add('contact');
+        newContact.id = users.users[u].username;
+        
+        newContact.onclick = function() { contactCard(this) }; 
+
+        const imgFrame = document.createElement('div');
+        imgFrame.classList.add('imgFrame');
+        const img = document.createElement('img');
+        img.src = '../assets/images/default-profile.jpg';
+        imgFrame.appendChild(img);
+        
+        newContact.appendChild(imgFrame);
+        
+        const h3 = document.createElement('h3');
+        h3.textContent = users.users[u].username;
+        newContact.appendChild(h3);
+        
+        contactList.appendChild(newContact);
+    }
+}
+
